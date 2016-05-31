d71594e7af846c2dc2548c3137ba67f7e587dceb
shiquan-lim <shiquan.lim@gmail.com>
2016-05-31 22:40:20 +0800

    finalised scope and legitimised app structure

--- src/render/scope.js
+++ src/render/scope.js
@@ -1,75 +1,11 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Classmentors Scope Diagram</title>
-</head>
-<script src="https://d3js.org/d3.v3.min.js"></script>
-<script src="chartpopup.js"></script>
-<link rel="stylesheet" type="text/css" href="bootstrap-3.3.6-dist/css/bootstrap.css">
-  <script src="jquery-2.2.3.js"></script>
-  <script src="bootstrap-3.3.6-dist/js/bootstrap.min.js"></script>
-  <script src="bootbox.js"></script>
-<meta charset="utf-8">
-<style>
-
-.node circle {
-  cursor: pointer;
-  stroke: #3182bd;
-  stroke-width: 1.5px;
-}
-
-.node text {
-  font: 10px sans-serif;
-  pointer-events: none;
-  text-anchor: middle;
-}
-
-line.link {
-  fill: none;
-  stroke: #9ecae1;
-  stroke-width: 1.5px;
-}
-
-text.shadow {
-    stroke: white;
-    stroke-width: 0.3px;
-    opacity: 1;
-}
-
-</style>
-<body>
-  <div class="container">
-  <div class="jumbotron">
-    <h2 class="text-center">Classmentors Interactive Scope Model</h2>
-    <p class="text-center">Modules to be completed are represented as nodes in the model</p></div>
-  <div class="row">
-    <div class="col-sm-4">
-      <h3 class="text-center">Hover Over</h3>
-      <p class="text-center">To show linkage to neighbouring modules</p>
-      <p class="text-center">Or drag to rearrange node layout</p>
-          </div>
-    <div class="col-sm-4">
-      <h3 class="text-center">Click Once</h3>
-      <p class="text-center">To collapse and expand modules. </p>
-      <p class="text-center">Relative node size is respective to the estimated diffculty of the module, with an additive hierarchy</p>
-    </div>
-    <div class="col-sm-4">
-      <h3 class="text-center">Double Click</h3>
-      <p class="text-center">To view module details</p>
-      <p class="text-center"></p>
-    </div>
-  </div>
-</div>
-
-
-<script>
-var width = 1150,
-    height = 700,
+function renderScope() {
+	var width = 1150,
+    height = 900,
     root;
 
 var force = d3.layout.force()
-    .linkDistance(110)
-    .charge(-150)
+    .linkDistance(95)
+    .charge(-200)
     .gravity(.05)
     .size([width, height])
     .on("tick", tick);
@@ -81,77 +17,70 @@ var svg = d3.select("body").append("svg")
 var link = svg.selectAll(".link"),
     node = svg.selectAll(".node");
 
-// d3.json("graph.json", function(error, json) {
-//   if (error) throw error;
-
-//   root = json;
-//   update();
-// });
-
 root = {
  "name": "Classmentors",
- "colour": "#1E0000",
+ "colour": "#F1D3BC",
  "details": "An interactive web portal for educators to test differring pedagogical approaches within the same classroom and collect data on the effectiveness of various approaches.",
  "children": [
   {
     "name": "Primary",
-    "colour": "#04202C",
+    "colour": "#ED4848",
     "details": "The necessary functions that must be implemented for Classmentors to serve it's purpose. These tasks will hence be executed with the highest level of priority.",
     "children": [
         {
       "name": "Class Session",
-      "colour": "#4F6457",
+      "colour": "#E05858",
       "details": "Create an instance of a classmentors class session, which includes the features extended as shown.",
       "children": [
         {
           "name": "Administrator",
-          "colour": "#004D47",
+          "colour": "#DF8181",
           "details": "Functions available to the session creator, usually the lecturer.",
           "children": [
             {
               "name": "Automated Mentor Assignment",
-              "colour": "#128277",
+              "colour": "#EBAFAF",
               "details": "When enabled, performs intra-class assignment between students for stragglers to get help from stronger students who have already completed their assigned exercises.",
               "size": 6
             },
             {
               "name": "Deleting Down Experiment",
-              "colour": "#128277",
+              "colour": "#EBAFAF",
               "details": "When enabled, this module applies a variation to the activity that the class is assigned, and recursively performs A/B testing-like procedure to discern the \"most effective\" variation of the activity.",
               "size": 12
             },
             {
               "name": "Team Creation",
-              "colour": "#128277",
+              "colour": "#EBAFAF",
               "details": "Either randomly or based on the content creator's input, splits the class into teams for performing activities in the later stages of the class.",
               "size": 4
             },
             {
               "name": "Automated Partitioning",
-              "colour": "#128277",
+              "colour": "#EBAFAF",
               "details": "This function is performed in tandem with team creation, but can be executed as a stand-alone as well. Well enabled, automatically breaks the class into two main segments to apply subsequent activity variance to.",
               "size": 5
             },
             {
               "name": "Create Session",
-              "colour": "#128277",
+              "colour": "#EBAFAF",
               "details": "Creates an instance of a Classmentors class session, from which subsequent activities are appended.",
               "size": 8
             },
             {
               "name": "Create Challenges",
-              "colour": "#128277",
+              "colour": "#EBAFAF",
               "details": "The content creator is able to create challenges for the class to participate in.",
               "children": [
                 {
                   "name": "Content Upload",
-                  "colour": "#52958B",
+                  "colour": "#F3DADA",
                   "details": "Allows upload of various rich media, from documents to links and videos.",
                   "size": 4
                 },
                 {
                   "name": "Content Builder",
-                  "colour": "#52958B",
+                  "colour": "#F3DADA",
                   "details": "Provides the content creator with various standarised templates from which to build event content for the class to participate in.",
                   "size": 10
                 }
@@ -159,7 +88,7 @@ root = {
             },
             {
               "name": "Configurable Event Ranking",
-              "colour": "#128277",
+              "colour": "#EBAFAF",
               "details": "Allows the content creator to adjust the weightage that aggregated events contribute to the total team score.",
               "size": 6
             }
@@ -167,36 +96,36 @@ root = {
         },
         {
           "name": "Student", 
-          "colour": "#004D47",
+          "colour": "#DF8181",
           "details": "Challenges, events, and tasks that students in the class may access and participate in.", 
           "children": [
             {
               "name": "Index Card",
-              "colour": "#128277",
+              "colour": "#EBAFAF",
               "details": "Students participating in this event may submit individial digital index cards, which may contain questions pertaining to the class or simply information that they wish to elicit from the content creator. The team is then able to internally filter the index cards before submission.",
               "size": 5
             },
             {
               "name": "Jeopardy",
-              "colour": "#128277",
+              "colour": "#EBAFAF",
               "details": "Students are able to participate in a game-like questionnaire event, which may be in the form of multiple choice or open ended questions. Elements such as wagering and repeated submission upon an errorneous answer are supported.",
               "size": 12
             },
             {
               "name": "MCQ",
-              "colour": "#128277",
+              "colour": "#EBAFAF",
               "details": "Students are given a multiple choice answer challenge, from which they select their answer and submit to the content creator.",
               "size": 6
             },
             {
               "name": "Journaling",
-              "colour": "#128277",
+              "colour": "#EBAFAF",
               "details": "Students are provided with an open ended text area field, where they input a text response to a given topic or question for submission.",
               "size": 3
             },
             {
               "name": "Code",
-              "colour": "#128277",
+              "colour": "#EBAFAF",
               "details": "Students are provided with a text area to submit code snippets or an upload option to upload a code-based file for submission in response to a given challenge.",
               "size": 8
             }
@@ -206,24 +135,24 @@ root = {
   },
   {
     "name": "Analytics",
-    "colour": "#4F6457",
+    "colour": "#E05858",
     "details": "Provide statistical and descriptive insight into metrics of the class session",
     "children": [
       {
         "name": "Report generation",
-        "colour": "#004D47",
+        "colour": "#DF8181",
         "details": "This module builds a report stating the descriptive analytic status of the events and surveys conducted within a particular Classmentors class session. This view will include information of individual students, teams, and class segments from the automated partitioning.",
         "size": 20
       },
       {
         "name": "Completion Statistics",
-        "colour": "#004D47",
+        "colour": "#DF8181",
         "details": "Completion statistics include deeper detail than the generated report. This module includes non-functional reports such as time taken for completion, relative speed and accuracy, and the results of the partitioned A/B testing.",
         "size": 20
       },
       {
         "name": "Raw Data Display",
-        "colour": "#004D47",
+        "colour": "#DF8181",
         "details": "Raw data display shows the raw results by means of javascript object notation (JSON) or comma seperated values (CSV).",
         "size": 4
       }
@@ -233,23 +162,23 @@ root = {
   },
   {
     "name": "Secondary",
-    "colour": "#500805",
+    "colour": "#F9C95B",
     "details": "These modules function in support of the primary modules. While good to have, they do not contribute standalone functionality, and hence will only be prioritised once primary modules have been completed.",
     "children": [
       {
     "name": "Usability",
-    "colour": "#9D331F",
+    "colour": "#FFDB5C",
     "details": "This module serves to increase the usability of existing primary functions by utilising metrics such as number of clicks to task completion and the goal of a single page view.",
     "children": [
       {
         "name": "Ease of Access",
-        "colour": "#BC6D4F",
+        "colour": "#FFF2BB",
         "details": "Ease of access focuses on aspects of usability such as intuitiveness of icons, number of clicks to task completion, degree of scroll-to-view, and number of pages to desired destination.",
         "size": 15
       },
       {
         "name": "Automated Field Completion",
-        "colour": "#BC6D4F",
+        "colour": "#FFF2BB",
         "details": "This module supports existing primary functions such as the \"Content Builder\" by initiailising intelligent defaults and recommendations to field inputs.",
         "size": 4
       }
@@ -257,24 +186,24 @@ root = {
   },
   {
     "name": "Data Visualisation",
-    "colour": "#9D331F",
+    "colour": "#FFDB5C",
     "details": "This module supports the \"Analytics\" primary module by providing a user friendly and interactive data representation view.",
     "children": [
       {
         "name": "Result Dashboard",
-        "colour": "#BC6D4F",
+        "colour": "#FFF2BB",
         "details": "Directly supports the \"Report Generation\" module by initialising a dashboard to complement the resultant information. Basic charts such as bar, pie, scatter, spline, and stacked bar will be supported.",
         "size": 20
       },
       {
         "name": "Data Export",
-        "colour": "#BC6D4F",
+        "colour": "#FFF2BB",
         "details": "Directly supports the \"Raw Data Display\" module by allowing export of the data to the user's local machine for purpose of forwarding or processing by third-party software.",
         "size": 6
       },
       {
         "name": "Advanced Charts",
-        "colour": "#BC6D4F",
+        "colour": "#FFF2BB",
         "details": "Renders charts not supported by the \"Result Dashboard\" module. Such charts include hierarchical visualisations such as sunburst models, and interactive timeline models.",
         "size": 15
       }
@@ -284,23 +213,23 @@ root = {
   },
   {
     "name": "Tertiary",
-    "colour": "#283655",
+    "colour": "#90AFC5",
     "details": "These modules are considered by the team to provide above-and-beyond functionality, and while they can be immensely beneficial to the portal, they have been deemed to be beyond the scope of client requirements. They will only be addressed should the project be far ahead of scheduele or in the event of a shift in demand from the client.",
     "children": [
       {
     "name": "Advanced Analytics",
-    "colour": "#4D648D",
+    "colour": "#A1D6E2",
     "details": "This module supports a higher degree of analytics than the \"Result Generation\" module.",
     "children": [
       {
         "name": "Aggregated Analytics",
-        "colour": "#90AFC5",
+        "colour": "#C4DFE6",
         "details": "This module aims to provide insights by means of summation, averaging, and statistical methods. Other higher-level analytics such as text mining and sentiment analysis in tandem with parallel processing frameworks can be implemented here as well.",
         "size": 30
       },
       {
         "name": "Recommendation System",
-        "colour": "#90AFC5",
+        "colour": "#C4DFE6",
         "details": "Implement predictive and prescriptive analytics by means of decision trees and machine learning to provide recommendations to the content creator as to how to conduct the Classmentors class session.",
         "size": 30
       }
@@ -308,18 +237,18 @@ root = {
   },
   {
     "name": "Third Party Support",
-    "colour": "#4D648D",
+    "colour": "#A1D6E2",
     "details": "Allow interaction with the developer.",
     "children": [
       {
         "name": "FAQ",
-        "colour": "#90AFC5",
+        "colour": "#C4DFE6",
         "details": "Create a repository of commonly asked questions pertaining to challenges and their answers.",
         "size": 6
       },
       {
         "name": "Request Submission",
-        "colour": "#90AFC5",
+        "colour": "#C4DFE6",
         "details": "Allow remote submission to the client (Professor Boesch and team) of questions or requests.",
         "size": 6
       }
@@ -381,7 +310,7 @@ function update() {
     smallestQty = d3.min(values);
   var scale = d3.scale.linear()
         .domain([smallestQty, largestQty])
-        .range([10, 60]);
+        .range([15, 50]);
 
   // Restart the force layout.
   force
@@ -416,9 +345,9 @@ function update() {
   nodeEnter.append("text")
       .attr("dy", ".35em")
       .text(function(d) { return d.name; })
-      .style("font-size", "12px") 
-      .attr("class", "shadow")
-      .attr("font-weight", "bold");
+      .style("font-size", "12px")
+      .attr("class", "shadow");
+      // .attr("font-weight", "bold");
 
   node.select("circle")
       .style("fill", function(d) {
@@ -509,6 +438,4 @@ function connectedNodes() {
         toggle = 0;
     }
 }
-</script>
-</body>
-</html>
+}
